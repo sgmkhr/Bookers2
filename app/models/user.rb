9d4c8d5114aa4543f5fa74734d0cd63c1ca9d519
sgmkhr <154421941+sgmkhr@users.noreply.github.com>
2024-04-03 11:14:56 +0000

    [fix]デバッグ

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,9 @@ class User < ApplicationRecord
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/noimage.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'noimage.jpg', content_type: 'image/jpeg')
-    else 
-      profile_image.variant(resize_to_limit: [width, height]).processed
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end 
+      profile_image.variant(resize_to_limit: [width, height]).processed
   end 
 
 end
